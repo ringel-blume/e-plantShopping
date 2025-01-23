bb5b132061467b655eec321b4c6d69e5cdddd379
ringel-blume <ringel.neni.blume@gmail.com>
2025-01-23 10:40:49 +0100

    HH

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => {
+      return total + item.cost * item.quantity; 
+    }, 0);
+  };
+
+  const handleCheckoutShopping = () => {
+    alert("Checkout functionality will be added later.");
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1
+   }
+  ));
   };
 
   const handleDecrement = (item) => {
-   
-  };
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 
+      }
+     ));
+    }
+   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ id: item.id }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
   };
 
   return (
